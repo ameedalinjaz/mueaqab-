aa54911c63ed23dd018d939a88e11118f4cbc142
ameedalinjaz <malkalmhmoud25@gmail.com>
2025-09-10 14:14:22 +0300

    Create  page.js

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ textAlign: "center", padding: "50px" }}>
+      <h1>مرحبًا بك في موقعي 🚀</h1>
+      <p>هذا اختبار للتأكد أن المشروع يعمل بشكل صحيح.</p>
+    </main>
+  );
+}
